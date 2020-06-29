dd1ee7f39decea9c912fdbe4f712c3255944e5fb
general-enquir <58457221+general-enquiry@users.noreply.github.com>
2020-06-29 22:44:04 +1000

    all

---
+++ site/templates/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <h1 class="info">?</h1>
+</head>
+<body>
+    <!-- <div class="h1">Observation,</div>
+    <div class="h1">Documentation,</div>
+    <div class="h1">Instruction &</div>
+    <div class="h1">Distribution.</div> -->
+    <h1 class="observation">Observation,</h1>
+    <h1 class="documentation">Documentation,</h1>
+    <h1 class="instruction">Instruction &</h1>
+    <h1 class="distribution">Distribution.</h1>
+
+
+</body>
+
+<footer>
+  <marquee scrollamount="10" direction="left" loop="10">We acknowledge that this website and it's content was developed on the land of the people of the Kulin Nations and we wish to pay our respects to their Elders, past and present.</marquee>
+</footer>
