a026920d2b20a642469e46464ac1d73464bd6ef5
Matt Lenz <matt@mattlenz.com>
2021-05-26 18:08:16 +1000

    Fix deploy scripts referencing People

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-ssh root@159.65.41.239 /bin/bash << EOF
+ssh root@SERVER_IP /bin/bash << EOF
   cd /var/www/html
   git fetch
   git reset --hard origin/master
-  cd xero
-  npm install
-  pm2 restart all
+  yarn
+  yarn run build
 EOF
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-rsync -r -p -t -u -z --exclude=".*" -P -h -i --delete root@159.65.41.239:/var/www/html/content ./
+rsync -r -p -t -u -z --exclude=".*" -P -h -i --delete root@SERVER_IP:/var/www/html/content ./
