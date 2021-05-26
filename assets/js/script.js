1c6e614b1da24bb1e8879c7c2ce0a7c8575d8e3f
Paul Mylecharane <hi@public-office.info>
2021-05-26 17:57:37 +1000

    add interact to vendor

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,48 +50,54 @@ $(document).ready(function() {
   }, delay)
 
   interact('.item')
-    .draggable({
-      onmove: window.dragMoveListener
-    })
-    .resizable({
-      preserveAspectRatio: true,
-      edges: { left: true, right: true, bottom: true, top: true }
-    })
-    .on('resizemove', function (event) {
-      var target = event.target,
-          x = (parseFloat(target.getAttribute('data-x')) || 0),
-          y = (parseFloat(target.getAttribute('data-y')) || 0);
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
 
-      // update the element's style
-      target.style.width  = event.rect.width + 'px';
-      target.style.height = event.rect.height + 'px';
+    listeners: {
+      move (event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
 
-      // translate when resizing from top or left edges
-      x += event.deltaRect.left;
-      y += event.deltaRect.top;
+        // update the element's style
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
 
-      target.style.webkitTransform = target.style.transform =
-          'translate(' + x + 'px,' + y + 'px)';
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left
+        y += event.deltaRect.top
 
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-      target.textContent = event.rect.width + '×' + event.rect.height;
-    });
+        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
 
-  function dragMoveListener (event) {
-      var target = event.target,
-          // keep the dragged position in the data-x/data-y attributes
-          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+      }
+    },
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
 
-      // translate the element
-      target.style.webkitTransform =
-      target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
 
-      // update the posiion attributes
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    }
+    inertia: true
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ]
+  })
 
 })
